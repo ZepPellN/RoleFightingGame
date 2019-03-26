6d33f73d381f283d9b77bb14f5f97ead5c77112c
Christine <njuzsq@outlook.com>
2019-03-27 01:21:30 +0800

    init

---
+++ src/main/java/com/nju/software/RoleFightingGame/RoleFightingGameApplication.java
@@ -0,0 +1,13 @@
+package com.nju.software.RoleFightingGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoleFightingGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoleFightingGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nju/software/RoleFightingGame/RoleFightingGameApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nju.software.RoleFightingGame;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RoleFightingGameApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
